313e44a722593a61df9ff56076171e43df8460a1
Erik Storås Sommer <erikssommer@gmail.com>
2023-03-22 14:29:32 +0100

    Added comments

--- model/model.py
+++ model/model.py
@@ -20,20 +20,17 @@ def df_to_x_y(df, window_size=5):
         y.append(label)
     return np.array(X), np.array(y)
 
-
+# Prepare the data
 window_size = 5
 X, y = df_to_x_y(measurement, window_size)
-print(X.shape, y.shape)
 
 # Split the data
 data_size = len(X)
 train_size = int(data_size * 0.7)
 
+# Train and test data
 X_train, y_train = X[:train_size], y[:train_size]
 X_test, y_test = X[train_size+1:], y[train_size+1:]
-
-
-print(X_train.shape, y_train.shape)
 
 # Build the model
 model = tf.keras.models.Sequential()
@@ -42,12 +39,15 @@ model.add(tf.keras.layers.LSTM(64))
 model.add(tf.keras.layers.Dense(8, activation='relu'))
 model.add(tf.keras.layers.Dense(1, activation='linear'))
 
+# Print the model summary
 model.summary()
 
+# Compile the model
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss=tf.keras.losses.MeanSquaredError(), 
               metrics=[tf.keras.metrics.RootMeanSquaredError()])
 
+# Train the model
 model.fit(X_train, y_train, epochs=1000)
 
 # Save the model
@@ -56,10 +56,11 @@ model.save('./models/model.h5')
 # Evaluate the model
 model = tf.keras.models.load_model('./models/model.h5')
 
-print(len(y_test))
+# Test the model
 test_pred = model.predict(X_test).flatten()
 test_result = pd.DataFrame(data={'Test pred': test_pred, 'Test actual': y_test})
 
+# Print the results
 print(test_result)
 
 # Plot the results
