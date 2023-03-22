971f92de660f934ce776aaf7e617e64ae2545163
Erik Storås Sommer <erikssommer@gmail.com>
2023-03-22 18:32:25 +0100

    Working on more plots

---
+++ analytics/src/main.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+# Load the data
+df = pd.read_csv('../data/dataset.csv', sep=',')
+df.index = pd.to_datetime(df['timestamp'], format='%Y-%m-%d %H:%M:%S')
+
+measurement = df['measurement']
+
+def df_to_x_y(df, window_size=5):
+    df_as_numpy = df.to_numpy()
+    timestamps = df.index[window_size:]
+    X = []
+    y = []
+    for i in range(len(df_as_numpy) - window_size):
+        row = [[x] for x in df_as_numpy[i:i + window_size]]
+        X.append(row)
+        label = df_as_numpy[i + window_size]
+        y.append(label)
+    return np.array(X), np.array(y), timestamps
+
+# Prepare the data
+window_size = 5
+X, y, timestamps = df_to_x_y(measurement, window_size)
+
+# Split the data
+data_size = len(X)
+train_size = int(data_size * 0.7)
+
+# Train and test data
+X_train, y_train = X[:train_size], y[:train_size]
+X_test, y_test = X[train_size+1:], y[train_size+1:]
+test_timestamps = timestamps[train_size+1:]
+
+# Build the model
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.InputLayer(input_shape=(window_size, 1)))
+model.add(tf.keras.layers.LSTM(64))
+model.add(tf.keras.layers.Dense(8, activation='relu'))
+model.add(tf.keras.layers.Dense(1, activation='linear'))
+
+# Print the model summary
+model.summary()
+
+# Compile the model
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+              loss=tf.keras.losses.MeanSquaredError(), 
+              metrics=[tf.keras.metrics.RootMeanSquaredError()])
+
+# Train the model
+model.fit(X_train, y_train, epochs=1000)
+
+# Save the model
+model.save('./models/model.h5')
+
+# Evaluate the model
+model = tf.keras.models.load_model('./models/model.h5')
+
+# Test the model
+test_pred = model.predict(X_test).flatten()
+test_result = pd.DataFrame(data={'Test pred': test_pred, 'Test actual': y_test, 'Timestamp': test_timestamps})
+training_actuals = pd.DataFrame(data={'Training actual': y_train, 'Timestamp': timestamps[:train_size]})
+
+# Print the results
+print(test_result)
+
+# Plot the results
+fig, ax = plt.subplots()
+# Set the figure size
+fig.set_size_inches(12.5, 6.5)
+
+ax.plot(test_result['Timestamp'], test_result['Test pred'], label='Test pred')
+ax.plot(test_result['Timestamp'], test_result['Test actual'], label='Test actual')
+
+# Set the format of the x-axis labels to display the timestamp in the format '%Y-%m-%d %H:%M:%S'
+date_fmt = '%Y-%m-%d %H:%M:%S'
+date_formatter = mdates.DateFormatter(date_fmt)
+ax.xaxis.set_major_formatter(date_formatter)
+plt.gcf().autofmt_xdate()
+plt.title('Test results')
+plt.xlabel('Timestamp')
+plt.ylabel('Measurement')
+plt.legend()
+
+# Plot the training and test data in the same plot
+fig, ax = plt.subplots()
+# Set the figure size
+fig.set_size_inches(12.5, 6.5)
+
+ax.plot(training_actuals['Timestamp'], training_actuals['Training actual'], label='Training actual')
+ax.plot(test_result['Timestamp'], test_result['Test actual'], label='Test actual')
+ax.plot(test_result['Timestamp'], test_result['Test pred'], label='Test pred')
+
+# Set the format of the x-axis labels to display the timestamp in the format '%Y-%m-%d %H:%M:%S'
+date_fmt = '%Y-%m-%d %H:%M:%S'
+date_formatter = mdates.DateFormatter(date_fmt)
+ax.xaxis.set_major_formatter(date_formatter)
+plt.gcf().autofmt_xdate()
+plt.title('Training and test results')
+plt.xlabel('Timestamp')
+plt.ylabel('Measurement')
+plt.legend()
+plt.show()
