4f73373c20be9af084d9bef470a9f659c1005413
Vetle Grim Hjelmtvedt <vetle.grim.hjelmtvedt@dnb.no>
2023-04-12 09:55:41 +0200

    boilerplate project

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import styles from './Header.css'
+
+
+function Header() {
+    return (
+        <div className={styles.container}>
+            <h1>Rendering</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import styles from './Homepage.css'
+import Header from '../components/Header'
+
+
+function Homepage() {
+    return (
+      <div className={styles.container}>
+        <Header />
+      </div>
+    );
+  }
+  
+  export default Homepage;
